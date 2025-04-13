70288353ce3f25410fc0059b144e13f5ab49ae6a
Zini Chakraborty <zinichakraborty@gmail.com>
2025-04-13 18:46:20 -0400

    add user profile store

---
+++ db/user_profile.py
@@ -0,0 +1,37 @@
+from dotenv import load_dotenv
+import os
+import redis
+import json
+
+load_dotenv()
+host = os.getenv("REDIS_HOST")
+port = os.getenv("REDIS_PORT")
+password = os.getenv("REDIS_PASSWORD")
+
+r = redis.Redis(
+    host=host,
+    port=port,
+    decode_responses=True,
+    username="default",
+    password=password,
+)
+
+def save_user_data(name, watched_movies, preferences):
+    if not name:
+        raise ValueError("User name is required")
+
+    user_data = {
+        "watched_movies": watched_movies,
+        "preferences": preferences
+    }
+
+    r.set(name, json.dumps(user_data))
+    print(f"Saved data for user: {name}")
+
+def get_user_data(name):
+    data = r.get(name)
+    if data:
+        return json.loads(data)
+    else:
+        print(f"No data found for user: {name}")
+        return None
